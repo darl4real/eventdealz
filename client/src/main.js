b65e213b6714e0aa26b84a38f2cb7db22553eb55
darl4real <darl4real@hotmail.com>
2018-08-14 16:41:02 +0200

    registration and authentication created

--- client/src/main.js
+++ client/src/main.js
@@ -1,15 +1,30 @@
+import 'vuetify/dist/vuetify.min.css'
+
+import App from './App'
+import Panel from '@/components/globals/Panel'
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import VueYouTubeEmbed from 'vue-youtube-embed'
+import Vuetify from 'vuetify'
 import router from './router'
+import store from '@/store/store'
+import { sync } from 'vuex-router-sync'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuetify)
+Vue.use(VueYouTubeEmbed)
+
+Vue.component('panel', Panel)
+
+sync(store, router)
+
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    store,
+    template: '<App/>',
+    components: { App }
 })
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,48 @@
-import Vue from 'vue'
+import CreateSong from '@/components/CreateSong'
+import EditEvent from '@/components/EditEvent'
+import Events from '@/components/Events/Index'
+import Login from '@/components/Login'
+import Register from '@/components/Register'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import ViewEvent from '@/components/ViewEvent/Index'
+import Vue from 'vue'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [{
+            path: '/register',
+            name: 'register',
+            component: Register
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: Login
+        },
+        {
+            path: '/events',
+            name: 'events',
+            component: Event
+        },
+        {
+            path: '/events/create',
+            name: 'events-create',
+            component: CreateEvent
+        },
+        {
+            path: '/events/:eventId',
+            name: 'event',
+            component: ViewEvent
+        },
+        {
+            path: '/events/:eventId/edit',
+            name: 'event-edit',
+            component: EditEvent
+        },
+        {
+            path: '*',
+            redirect: 'events'
+        }
+    ]
 })
